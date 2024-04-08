3a761b42057137d71184686f236f71b0893ad7dc
MTrzpis4 <mateusztrzpis441@gmail.com>
2024-04-08 08:18:37 +0200

    dodalem jedzenie

--- lista_3AP.py
+++ lista_3AP.py
@@ -1,10 +1,12 @@
 
-klasa_3AP = {}
+klasa_3AP = {
+   "30": "Pizza",
+}
 
 
 osoba = {
-    '0': 'Imie Nazwisko'
-    '30':'Mateusz Trzpis'
+    '0': 'Imie Nazwisko',
+    '30':'Mateusz Trzpis',
 }
-
+print(klasa_3AP)
 print(osoba)
