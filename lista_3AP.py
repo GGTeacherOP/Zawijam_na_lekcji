39fa18b8bc64bb42495c898836ca6d3d5ca7bae3
unknown <adrian.kusek2006@gmail.com>
2024-04-08 08:28:58 +0200

    death

--- lista_3AP.py
+++ lista_3AP.py
@@ -1,10 +1,12 @@
 
-klasa_3AP = {}
+klasa_3AP = {
+    '17': 'Mieso ludzkie',
+}
 
 
 osoba = {
-    '0': 'Imie Nazwisko'
-    '17': 'Adrian Kusek'
+    '0': 'Imie Nazwisko',
+    '17': 'Adrian Kusek',
 }
 
 print(osoba)
